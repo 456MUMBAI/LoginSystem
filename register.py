ff7fe950ea1fa7522475c036507ebef14676c1f5
RITU <ritu.kansal456@gmail.com>
2021-10-19 11:52:11 +0530

    mysql connection changed to new database.

--- register.py
+++ register.py
@@ -1,2 +1,6 @@
-print("hello")
-print("welcome all")
+#registration file
+import mysql.connector as ms
+
+mycon = ms.connect(host='db4free.net', user='clairie', passwd='education', charset='utf8', database='skytouch')
+if mycon.is_connected():
+    print('connected!')
