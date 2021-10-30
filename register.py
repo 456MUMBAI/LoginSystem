27ece469c02f6550450d4476d6ac37117479f82d
semes23 <86822697+semes23@users.noreply.github.com>
2021-10-30 10:07:16 +0530

    Update register.py

--- register.py
+++ register.py
@@ -1,9 +1,4 @@
 # registration file
-import mysql.connector as ms
-
-mycon = ms.connect(host='db4free.net', user='clairie', passwd='education', charset='utf8', database='skytouch')
-if mycon.is_connected():
-    print('connected!')
 
 import mysql.connector as ms
 import random
