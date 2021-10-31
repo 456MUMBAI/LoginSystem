ead0a863bda378f279692bd80ed5515aff2ed2b2
RITU <ritu.kansal456@gmail.com>
2021-10-31 14:42:00 +0530

    errors resolved. connection done b/w 3 files. pls make the menu in main file onwards.

---
+++ main.py
@@ -0,0 +1,18 @@
+#menu
+import login
+import register
+import mysql.connector as ms
+mycon = ms.connect(host='db4free.net', user='clairie', passwd='education', charset='utf8', database='skytouch')
+
+print('connected!')
+mycursor = mycon.cursor()
+mycursor.execute('select * from login_info;')
+login_data = mycursor.fetchall()
+mycursor.execute("select * from reg;")
+reg_data = mycursor.fetchall()
+print('WELCOME TO SKYTOUCH!!')
+ch=input('> ')
+if ch=='1' :
+    login.verify()
+elif ch=='2':
+    register.fun()
--- register.py
+++ register.py
@@ -1,174 +1,150 @@
 # registration file
-import mysql.connector as ms
+# import login
+import main
+def mobile():
+    """to input mobile no. of user."""
+    loop1 = "y"
+    while loop1 == "y":
+        mb=input("Enter your mobile number= ")
+        if len(mb) == 10:
+            if mb.isdigit():
+                mobile = mb
+                loop1 = "n"
 
-mycon = ms.connect(host='db4free.net', user='clairie', passwd='education', charset='utf8', database='skytouch')
-if mycon.is_connected():
-    print('connected!')
-
-import mysql.connector as ms
-import random
-
-mycon = ms.connect(host="db4free.net", user="clairie", passwd="education", charset="utf8", database="skytouch")
-if mycon.is_connected():
-    print("connected")
-    mycursor = mycon.cursor()
-    mycursor.execute("select * from reg")
-    data = mycursor.fetchall()
+                return mobile
+        else:
+            print("Invalid mobile number")
+            loop1 = "y"
 
 
-    def mobile():
-        """to input mobile no. of user."""
-        loop1 = "y"
-        while loop1 == "y":
-            mb = input("Enter your mobile number= ")
-            if len(mb) == 10:
-                if mb.isdigit():
-                    mobile = mb
-                    loop1 = "n"
+def username():
+    """to input username of user"""
+    loop2 = "y"
+    while loop2 == "y":
+        uname=input("Enter your username= ")
+        length = len(uname)
 
-                    return mobile
+        for i in main.reg_data:
+            if uname == i[2]:
+                print("This username already exists")
+                loop2 = "y"
+
             else:
-                print("Invalid mobile number")
-                loop1 = "y"
-
-
-    def username():
-        """to input username of user"""
-        loop2 = "y"
-        while loop2 == "y":
-            uname = input("Enter your username= ")
-            length = len(uname)
-
-            for i in data:
-                if uname == i[2]:
-                    print("This username already exists")
+                if length == 0:
+                    print("Enter a valid username")
                     loop2 = "y"
-
                 else:
-                    if length == 0:
-                        print("Enter a valid username")
-                        loop2 = "y"
-                    else:
-                        user = uname
-                        loop2 = "n"
-                        return user
+                    user = uname
+                    loop2 = "n"
+                    return user
 
 
-    def password():
-
-        loop3 = "y"
-        while loop3 == "y":
-            pswd = input("Enter a password= ")
-            length = len(pswd)
-            if length >= 8:
-                if pswd.isalnum():
-                    passwd = pswd
-                    loop3 = "n"
-                    return passwd
-                elif length > 20:
-                    print("Password should be less than 20 characters")
-                    loop3 = "y"
-                else:
-                    print("Password should consist of only digits and alphabets")
-                    loop3 = "y"
-
+def password():
+    """to change / add password"""
+    print("Password should not be less than 8 characters and more than 20 characters")
+    print("Password should consist of only digits and alphabets")
+    print("No special characters allowed")
+    loop3 = "y"
+    while loop3 == "y":
+        pswd=input("Enter a password= ")
+        length = len(pswd)
+        if length >= 8:
+            if pswd.isalnum():
+                passwd = pswd
+                loop3 = "n"
+                return passwd
+            elif length > 20:
+                print("Password should be less than 20 characters")
+                loop3 = "y"
             else:
-                print("Password should not be less than 8 characters")
+                print("Password should consist of only digits and alphabets")
                 loop3 = "y"
 
-
-    def vc():
-        """To generate unique vc number for every new customer."""
-        vt = random.randrange(100000, 100000000000)
-        for i in data:
-            if vt == i[0]:
-                vc = random.randrange(100000, 1000000000)
-                return vc
-            else:
-                vc = vt
-                return vc
-
-
-    def name():
-        """To input name of the user"""
-        loop = "y"
-        while loop == "y":
-            name = input("Enter your fullname=")
-            if len(name) == 0:
-                print("Please enter a valid name")
-                loop = "y"
-            elif name == " ":
-                print("Please enter a valid name")
-                loop = "y"
-            else:
-                loop = "n"
-                return name
-
-
-    def place():
-        """to input place"""
-        while True:
-            fav_place = input('Enter your favourite place: ')
-            if len(fav_place) == 0:
-                print('Please enter valid favourite place.')
-            elif fav_place.isspace():
-                print('Please enter a valid favourite place.')
-            else:
-                break
-        return fav_place
-
-
-    def color():
-        """to input color"""
-        while True:
-            fav_color = input('Enter your favourite color: ')
-            if len(fav_color) == 0:
-                print('Please enter valid favourite color.')
-            elif fav_color.isspace():
-                print('Please enter a valid favourite color.')
-            else:
-                break
-        return fav_color
-
-
-    def pet():
-        """to input pet"""
-        while True:
-            fav_pet = input('Enter your favourite pet: ')
-            if len(fav_pet) == 0:
-                print('Please enter valid favourite pet.')
-            elif fav_pet.isspace():
-                print('Please enter a valid favourite pet.')
-            else:
-                break
-        return fav_pet
-
-
-    choice = "y"
-    while choice == "y":
-        cc = int(input("Enter your country code= "))
-        if cc == 91:
-            name = name()
-            mobile = mobile()
-            user = username()
-            fav_place = place()
-            fav_color = color()
-            fav_pet = pet()
-
-            print("Password should not be less than 8 characters and more than 20 characters")
-            print("Password should consist of only digits and alphabets")
-            print("No special characters allowed")
-            passwd = password()
-
-            vc = vc()
-            mail = input("Enter email= ")
-
-            query = f"insert into reg values('{vc}','{name}','{user}','{mobile}','{passwd}','{mail}', '{fav_place}','{fav_color}','{fav_pet}')"
-            mycursor.execute(query)
-            mycon.commit()
-
-            choice = input("Do you want to register another user(y/n): ").lower()
-
-
         else:
-            print("DTH service available only in india")
+            print("Password should not be less than 8 characters")
+            loop3 = "y"
+def vc():
+    """To generate unique vc number for every new customer."""
+    import random
+    vt = random.randrange(100000, 100000000000)
+    for i in main.login_data:
+        if vt == i[0]:
+            vt = random.randrange(100000, 1000000000)
+    else:
+        return vt
+
+
+def name():
+    """To input name of the user"""
+    loop = "y"
+    while loop == "y":
+        name=input("Enter your fullname= ")
+        if len(name) == 0:
+            print("Please enter a valid name.")
+            loop = "y"
+        elif name == " ":
+            print("Please enter a valid name.")
+            loop = "y"
+        else:
+            loop = "n"
+            return name
+
+
+def place():
+    """to input place"""
+    while True:
+        fav_place=input('Enter your favourite place: ')
+        if len(fav_place) == 0:
+            print('Please enter valid favourite place.')
+        elif fav_place.isspace():
+            print('Please enter a valid favourite place.')
+        else:
+            break
+    return fav_place
+
+
+def color():
+    """to input color"""
+    while True:
+        fav_color=input('Enter your favourite color: ')
+        if len(fav_color) == 0:
+            print('Please enter valid favourite color.')
+        elif fav_color.isspace():
+            print('Please enter a valid favourite color.')
+        else:
+            break
+    return fav_color
+
+
+def pet():
+    """to input pet"""
+    while True:
+        fav_pet=input('Enter your favourite pet: ')
+        if len(fav_pet) == 0:
+            print('Please enter valid favourite pet.')
+        elif fav_pet.isspace():
+            print('Please enter a valid favourite pet.')
+        else:
+            break
+    return fav_pet
+
+def fun():
+
+    print('##REGISTRATION WINDOW##')
+    print('Dear Customer, please note our DTH service is only available in INDIA.')
+    nm=name()
+    uname=username()
+    passwd=password()
+    mb=mobile()
+    mail=input("Enter email= ")
+    print('Please provide the following details that will be required to change your password if you forget it in future.')
+    fav_place=place()
+    fav_color=color()
+    fav_pet=pet()
+    VC=vc()
+
+    query = f"insert into reg values('{VC}','{nm}','{uname}','{mb}','{passwd}','{mail}', '{fav_place}','{fav_color}','{fav_pet}')"
+    main.mycursor.execute(query)
+    main.mycon.commit()
+    print('##THANK YOU FOR REGISTERING WITH US!!##')
+
