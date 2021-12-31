ed68eb8b532835d893a2b6b583fd4d3a4c2e7c50
RITU <ritu.kansal456@gmail.com>
2021-12-31 16:31:36 +0530

    hohhuj

--- home.py
+++ home.py
@@ -1154,10 +1154,10 @@ def transaction(total_amount,User):
             transaction(total_amount,User)
         else:
 
-            Card_no= card_no()
-            Card_Holder=cardholder()
-            Exp_date=expiry_date()
-            CVV=cvv()
+            card_no()
+            cardholder()
+            expiry_date()
+            cvv()
             print(f'Your total amount is= Rs. {total_amount}')
             print(f'Press 1 to pay Rs. {total_amount}')
             print('Press any key to skip the transaction, you can pay later by logging into your account')
@@ -1178,7 +1178,8 @@ def transaction(total_amount,User):
 
                 menu()
     elif ch=="3":
-        print("UPI AutoPay is a new way to pay with UPI that will charge you automatically every month. That way you’ll never miss out on your shows and movies.")
+        print("UPI AutoPay is a new way to pay with UPI that will charge you automatically every month. "
+              "That way you’ll never miss out on your shows and movies.")
         print("Now accepting: @upi, @paytm, @ibl, @axl, @ybl, @apl")
         upi=input("enter you UPI Id".capitalize())
         print("press 1 to confirm your payment else press any key to edit")
